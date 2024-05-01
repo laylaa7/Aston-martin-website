70b217694a1af3f777a4c73282dab869d7737667
nourisded <nouramgad05@gmail.com>
2024-05-01 07:56:52 +0300

    still progress: function to answer the button clicked

---
+++ chatbox.js
@@ -0,0 +1,7 @@
+document.getElementById('ch1-1').onclick = function() {
+    var chatContainer = document.getElementById('right-part');
+    chatContainer.innerHTML = "We Have DBS, DB12, VALOUR, VANTAGE, DBX707";
+}
+ch1.addEventListener('click', function() {
+    document.getElementById('right-part').innerHTML = "bananas"
+})
