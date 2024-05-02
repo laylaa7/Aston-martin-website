76f735b6065ce32aad19ad7f08e3972ac4a214fc
laylaa7 <148385233+laylaa7@users.noreply.github.com>
2024-05-02 13:58:36 +0300

    implementing config page

---
+++ DBSconfig.js
@@ -0,0 +1,44 @@
+// // const carImage = document.getElementById('car-image');
+// // const colorOptions = document.querySelectorAll('#color-options input[type="radio"]');
+
+// // colorOptions.forEach(option => {
+// //   option.addEventListener('change', () => {
+// //     const color = option.value;
+// //     const imagePath = `DBSimg/DBS-${color}.jpeg`;
+// //     carImage.src = imagePath;
+// //   });
+// // });
+// const carImage = document.getElementById('background-image');
+// const colorOptions = document.querySelectorAll('#color-options input[type="image"]');
+
+// colorOptions.forEach(option => {
+//   option.addEventListener('click', () => {
+//     const color = option.value;
+//     const imagePath = `DBSimg/DBS-${color}.jpeg`;
+//     carImage.src = imagePath;
+//   });
+// });
+function openExteriorMenu() {
+    document.getElementById("exterior-menu").style.display = "block";
+    document.getElementById("interior-menu").style.display = "none";
+    document.getElementById("sidebar").style.display="none";
+  }
+  
+  function openInteriorMenu() {
+    document.getElementById("interior-menu").style.display = "block";
+    document.getElementById("exterior-menu").style.display = "none";
+    document.getElementById("sidebar").style.display="none";
+  }
+  
+  function changeBackgroundImage(color) {
+    const backgroundImage = document.getElementById("background-image");
+    backgroundImage.style.backgroundImage = `url('DBSimg/DBS-${color}.jpeg')`;
+  }
+
+
+const backButton = document.querySelector('.back-button');
+backButton.addEventListener('click', () => {
+  document.getElementById("interior-menu").style.display = "none";
+  document.getElementById("exterior-menu").style.display = "none";
+  document.getElementById("sidebar").style.display = "block";
+});
