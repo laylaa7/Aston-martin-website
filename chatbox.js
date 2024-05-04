31cd03424767f7fa6994d778f036e57377c87ef2
nourisded <nouramgad05@gmail.com>
2024-05-04 15:57:43 +0300

    menutoggle updated

--- chatbox.js
+++ chatbox.js
@@ -9,9 +9,21 @@
     }
     document.getElementById('ch2').onclick = function() {
        
-        document.getElementById('chat').innerHTML += "<p class=\"user-message\">Models Available</p>";
+        document.getElementById('chat').innerHTML += "<p class=\"user-message\">Test Drive Form </p>";
+       
+        setTimeout(function() {
+        document.getElementById('chat').innerHTML +="<p class=\"bot-message\">Upon selecting your preferred vehicle, we'll deliver it to your address for a trial before finalizing the purchase. To proceed, kindly fill out the form by clicking here.</p>";
+         }, 800);
+    }
+    document.getElementById('ch3').onclick = function() {
+       
+        document.getElementById('chat').innerHTML += "<p class=\"user-message\">About us</p>";
        
 
-        document.getElementById('chat').innerHTML +="<p class=\"bot-message\">We Have DBS, DB12, VALOUR, VANTAGE, DBX707</p>";
+        document.getElementById('chat').innerHTML +="<p class=\"bot-message\">Aston Martin’s vision is to be the world’s most desirable, ultra-luxury British brand, creating the most exquisitely addictive performance cars.Founded in 1913 by Lionel Martin and Robert Bamford , Click here to know more</p>";
         
+    }
+    const menuToggle=document.querySelector('.menuToggle');
+    menuToggle.onclick = function(){
+        menuToggle.classList.toggle('active');
     }
