7fe148b6a4912a293af9f90d9749b53b4db7cc93
nourisded <nouramgad05@gmail.com>
2024-05-07 01:58:04 +0300

    updated the chatbox error

--- chatbox.js
+++ chatbox.js
@@ -1,5 +1,5 @@
 
-    document.getElementById('ch').onclick = function() {
+    document.getElementById('ch1').onclick = function() {
        
         document.getElementById('chat').innerHTML += "<p class=\"user-message\">Models Available</p>";
        
