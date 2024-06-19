4908e9414e7cc0897f82d5bc6b304aab23ac8407
nour-bassem <Nour2200080@miuegypt.edu.eg>
2024-06-19 13:06:34 +0300

    database connected

--- index.js
+++ index.js
@@ -2,24 +2,66 @@ const express = require('express')
 var cors = require('cors')
 const path = require('path')
 
-// const session = require('express-session')
-// const fileUpload = require('express-fileupload');
+const mongoose = require('mongoose')
+const User= require('./models/database')
 
 const app = express()
 
-// const mongoose = require('mongoose')
+const dbURI = 'mongodb+srv://dbUser:nourPassword@cluster0.t2qbiv0.mongodb.net/data_History?retryWrites=true&w=majority&appName=Cluster0';
 
-// const dbURI = 'mongodb+srv://dbUser:nourPassword@cluster0.t2qbiv0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(dbURI)
+  .then(result => {
+    console.log("Connected to MongoDB");
+    app.listen(8080, () => {
+      console.log('Server is running on port 8080');
+    });
+  })
+  .catch(err => console.log(err));
 
-// mongoose.connect(dbURI)
-//   .then(result => app.listen(8080))
-//   .catch(err => console.log(err));
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+//mongo code
+app.get('/index-database-write', (req,res)=>{
+    const user=new User({
+        title:'new user 2',
+        snippet:'user email',
+        body:'user info'
+    });
+
+    user.save()
+       .then((result)=>{
+         console.log('write to database done');
+         res.send(result)
+       })
+       .catch((eer)=>{
+        console.log(err);
+       });
+})
+
+app.get('/index-database-read', (req,res)=>{
+    User.find()
+    .then((result)=>{
+        console.log('read from database done');
+        res.send(result)
+      })
+      .catch((eer)=>{
+       console.log(err);
+      });
+})
+
+app.get('/index-database-readSingle', (req,res)=>{
+    User.findById('6672aa1fca6a32995f7f99c8')
+    .then((result)=>{
+        console.log('read single record from database done');
+        res.send(result)
+      })
+      .catch((eer)=>{
+       console.log(err);
+      });
+})
 
 // load routes
 const modelsRoutes = require('./src/routes/modelsRoutes');
@@ -47,6 +89,7 @@ app.get("/src/views/adminLandingPage.html", (req,res) => {
 })
 
 
-app.listen(8080, function () {
-    console.log('CORS-enabled web server listening on port 80')
-})
+// app.listen(8080, function () {
+//     console.log('Server is running on port 8080')
+// })
+
