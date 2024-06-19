8832c8a18796810b209530d34a26dd7124e970f5
AbdelrahmanYasser01 <110674412+AbdelrahmanYasser01@users.noreply.github.com>
2024-06-20 00:37:38 +0300

    connecting htmls to nodejs

--- index.js
+++ index.js
@@ -20,6 +20,31 @@ app.get("src/views/models.html", (req,res) => {
 app.get("/src/views/reservation.html", (req,res) => {
     res.sendFile(path.join(__dirname, 'src/views/reservation.html'))
 })
+
+app.get("/src/views/testDrive.html", (req,res) => {
+    res.sendFile(path.join(__dirname, 'src/views/testDrive.html'))
+})
+
+app.get("/src/views/model1.html", (req,res) => {
+    res.sendFile(path.join(__dirname, 'src/views/model1.html'))
+})
+
+app.get("/src/views/model2.html", (req,res) => {
+    res.sendFile(path.join(__dirname, 'src/views/model2.html'))
+})
+
+app.get("/src/views/userHistory.html", (req,res) => {
+    res.sendFile(path.join(__dirname, 'src/views/userHistory.html'))
+})
+
+app.get("/src/views/adminDashProduct.html", (req,res) => {
+    res.sendFile(path.join(__dirname, 'src/views/adminDashProduct.html'))
+})
+
+app.get("/src/views/adminDashUser.html", (req,res) => {
+    res.sendFile(path.join(__dirname, 'src/views/adminDashUser.html'))
+})
+
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, 'src/index.html'))
 })
