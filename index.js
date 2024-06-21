82eaac036f10084b126372889160749ede5e6eba
AbdelrahmanYasser01 <110674412+AbdelrahmanYasser01@users.noreply.github.com>
2024-06-21 23:37:47 +0300

    index.js for mongoDB

--- index.js
+++ index.js
@@ -3,30 +3,27 @@ var cors = require('cors')
 const path = require("path")
 var app = express()
 
+require('dotenv').config();
+const mongoose = require('mongoose');
+const carRoutes = require('./routes/carRoutes');
 
 app.use(cors())
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//api
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 
-
-
-
-const dialogflowConfig = {
-  credentials: {
-    client_email: 'YOUR_CLIENT_EMAIL',
-    private_key: 'YOUR_PRIVATE_KEY',
-  },
-  project_id: 'YOUR_PROJECT_ID',
-};
 //load routes
 const modelsRoutes = require('./src/routes/modelsRoutes')
 const adminDashRoutes = require('./src/routes/adminDashRoutes')
-
-
-
+app.use('/api', carRoutes);
 app.use('/models', modelsRoutes)
 app.use('/admin' , adminDashRoutes)
 
@@ -45,8 +42,6 @@ app.get("/src/views/testdrive.html", (req,res) => {
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, 'src/index.html'))
 })
-
-
 
 app.listen(8080, function () {
     console.log('CORS-enabled web server listening on port 80')
