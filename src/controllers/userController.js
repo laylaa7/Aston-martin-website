4b010256fe7e865c4fefc53e5f8fd2648c690b84
laylaa7 <148385233+laylaa7@users.noreply.github.com>
2024-06-23 09:29:30 +0300

    editing final

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const Users = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 require('dotenv').config();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -17,7 +17,7 @@
  const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // router.post('/signup', userController.signup);
 // router.post('/verify-otp', userController.verifyOtp);
